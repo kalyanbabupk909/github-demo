d2a020ce0c885fd5cff6f1273cb4c5eb136f771d
Manikanta Dasari <kalyanbabupk909@gmail.com>
2022-09-09 22:02:20 +0530

    terraform files has been added

---
+++ terraform-ex/main.tf
@@ -0,0 +1,4 @@
+resource "local_file" "rand_res" {
+filename = "explicit.txt"
+content = "I love terraform "
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,31 @@
-kalyan babu ummadisetty
+
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.35.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "qwiklabs-gcp-02-06a3ae9ea2e6"
+ 
+  # Configuration options
+}
+
+
+resource "google_storage_bucket" "auto-expire" {
+  name          = "auto-expiring-bucket-ukb"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
